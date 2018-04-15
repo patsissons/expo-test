027442a85d38dc978624862e89c6adda8b665fbb
Pat Sissons <patricksissons@gmail.com>
2018-04-15 16:53:35 -0700

    converting tests to enzyme

--- test/components/demo/DemoView.test.tsx
+++ test/components/demo/DemoView.test.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { create } from 'react-test-renderer';
+import { shallow } from 'enzyme';
+import { Button } from 'react-native';
 import { DemoView } from '../../../src/components/demo/DemoView';
 
 describe('DemoView', () => {
@@ -8,23 +9,66 @@ describe('DemoView', () => {
     const component = (<DemoView />);
 
     // Act
-    const result = create(component).toJSON();
+    const result = shallow(component);
 
     // Assert
     expect(result).toMatchSnapshot();
+    expect(result.state()).toEqual({ count: 0 });
   });
 
   it('supports injecting custom initial count', () => {
     // Arrange
     const initialCount = 123;
-    const app = (<DemoView initialCount={ initialCount } />);
+    const component = (<DemoView initialCount={ initialCount } />);
 
     // Act
-    const result = create(app).root;
-    const state = result.findByProps({ testID: 'state' }).props.children;
+    const result = shallow(component);
 
     // Assert
-    expect(result.props).toEqual({ initialCount });
-    expect(state).toMatch(`${ initialCount }`);
+    expect(result.state()).toEqual({ count: initialCount });
+  });
+
+  it('renders the count state', () => {
+    // Arrange
+    const initialCount = 123;
+    const component = (<DemoView initialCount={ initialCount } />);
+
+    // Act
+    const result = shallow(component);
+
+    // Assert
+    expect(result
+      .find({ testID: 'state' })
+      .children()
+      .text(),
+    ).toMatch(new RegExp(`${ initialCount }$`));
+  });
+
+  it('increments the count state after pushing the button', () => {
+    // Arrange
+    const component = (<DemoView />);
+
+    // Act
+    const result = shallow(component);
+    result.find(Button).simulate('press');
+
+    // Assert
+    expect(result.state()).toEqual({ count: 1 });
+  });
+
+  it('renders the updated count state', () => {
+    // Arrange
+    const component = (<DemoView />);
+
+    // Act
+    const result = shallow(component);
+    result.find(Button).simulate('press');
+
+    // Assert
+    expect(result
+      .find({ testID: 'state' })
+      .children()
+      .text(),
+    ).toMatch(/1$/);
   });
 });
