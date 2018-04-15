03ca9675a894ce9e3c0a38cf78b14c05981190f4
Pat Sissons <patricksissons@gmail.com>
2018-04-15 13:16:15 -0700

    updating react namespace imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 import '../types';
 import { DemoView } from './components/demo/DemoView';
 
--- src/components/demo/DemoView.tsx
+++ src/components/demo/DemoView.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 
 export namespace DemoView {
