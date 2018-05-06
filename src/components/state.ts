5f5b086e0a2ad65c6620857a58e41627dde2369f
Pat Sissons <patricksissons@gmail.com>
2018-05-05 17:48:45 -0700

    adding state definitions for app context

--- src/components/state.ts
+++ src/components/state.ts
@@ -1,4 +1,18 @@
 import { NavigationState } from 'react-navigation';
+import { LocalizationMap } from '../locale';
+import { Env } from '../utils';
+
+export interface LocalizationContext {
+  locale: string;
+  localization: LocalizationMap;
+}
+
+export interface EnvContext {
+  env: Partial<Env>;
+}
+
+export interface ContextState extends LocalizationContext, EnvContext {
+}
 
 export interface DemoState {
   count: number;
@@ -12,6 +26,7 @@ export interface LoginState {
 }
 
 export interface Store {
+  context: ContextState;
   navigation: NavigationState;
   main: MainState;
   login: LoginState;
