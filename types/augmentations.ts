ead3e12eb509949f8aca673da56520efff5a0b63
Pat Sissons <patricksissons@gmail.com>
2018-05-05 17:45:12 -0700

    augmenting expo types to include DangerZone

--- types/augmentations.ts
+++ types/augmentations.ts
@@ -7,3 +7,16 @@ declare global {
     bind<T extends Function>(this: T, thisArg: any, ...argArray: any[]): T;
   }
 }
+
+import 'expo';
+declare module 'expo' {
+  export namespace DangerZone {
+    export namespace Localization {
+      export function getCurrentDeviceCountryAsync(): Promise<string>;
+      export function getCurrentLocaleAsync(): Promise<string>;
+      export function getCurrentTimeZoneAsync(): Promise<string>;
+      export function getISOCurrencyCodesAsync(): Promise<string>;
+      export function getPreferredLocalesAsync(): Promise<Array<string>>;
+    }
+  }
+}
