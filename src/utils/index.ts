3b34b8b8f7b228ef7dedc368b6a13990a101d04a
Pat Sissons <patricksissons@gmail.com>
2018-05-06 16:10:36 -0700

    refactoring type utils into its own module

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,17 +1,4 @@
 export * from './env';
+export * from './debug';
+export * from './types';
 export * from './typeguards';
-
-export type TypeNameMap<T> = { readonly [ TypeName in keyof T ]: TypeName };
-
-export function getTypeNames<T>(obj: T) {
-  return Object.keys(obj) as Array<keyof T>;
-}
-
-export function createTypeNameMap<T>(types: T): TypeNameMap<T> {
-  return getTypeNames(types)
-    .filter(x => isNaN(parseInt(x)))
-    .reduce(
-      (m, x) => { m[x] = x; return m; },
-      {} as any,
-    );
-}
